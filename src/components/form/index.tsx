6bc46b91141cf9808265d11667069c1d2b05a01d
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-12-20 11:45:52 +0600

    manage product sceleton added

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -2,3 +2,4 @@ export { default as ReimentForm } from "./ReimentForm";
 export { default as ReimentInput } from "./ReimentInput";
 export { default as ReimentSelect } from "./ReimentSelect";
 export { default as ReimentTextArea } from "./ReimentTextArea";
+export { default as RementMultiSelect } from "./RementMultiSelect";
--- src/pages/ManageProducts.tsx
+++ src/pages/ManageProducts.tsx
@@ -1,11 +1,36 @@
+import { Button } from "@/components/ui/button";
+
 const ManageProducts = () => {
+  let content = null;
+
   return (
-    <div className="ManageProductsContainer">
-      <h1> ManageProducts</h1>
-      <h1> ManageProducts</h1>
-      <h1> ManageProducts</h1>
-      <h1> ManageProducts</h1>
-      <h1> ManageProducts</h1>
+    <div className="ManageProductContainer">
+      <div className="ManageProductWrapper bg-gray-100 border border-gray-300  shadow rounded-md p-3">
+        <h3 className="brand text-2xl font-medium mb-4 "> Manage Product </h3>
+
+        <Button
+          onClick={() => (window.location.href = "/products/add")}
+          className="mb-4 bg-prime100 hover:bg-prime100 cursor-pointer"
+        >
+          Add Product
+        </Button>
+
+        <table className="w-full">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Sizes</th>
+              <th>Colors</th>
+              <th>Material</th>
+              <th>Stock</th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>{content}</tbody>
+        </table>
+      </div>
     </div>
   );
 };
